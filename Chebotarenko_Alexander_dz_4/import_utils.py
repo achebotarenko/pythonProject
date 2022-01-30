3ba751a06f19df6422b2b2caf6e6878729a47fc3
achebotarenko <90093648+achebotarenko@users.noreply.github.com>
2022-01-30 19:37:46 +0300

    Add files via upload

---
+++ Chebotarenko_Alexander_dz_4/import_utils.py
@@ -0,0 +1,5 @@
+import utils
+
+utils.currency_rates('USD')
+utils.currency_rates('eur')
+utils.currency_rates('Aud')
---
+++ Chebotarenko_Alexander_dz_4/utils.py
@@ -0,0 +1,23 @@
+def currency_rates(currency):
+
+    """
+    get the rate of currency by code from www.cbr.ru for RUB
+    :param currency:
+    :return: rate
+    """
+
+    from requests import get
+    response = get('http://www.cbr.ru/scripts/XML_daily.asp').text
+    charcode = [el_charcode.split('</CharCode>')[0] for el_charcode in response.split('<CharCode>')[1:]]
+    nominal = [el_nominal.split('</Nominal>')[0] for el_nominal in response.split('<Nominal>')[1:]]
+    value = [el_value.split('</Value>')[0] for el_value in response.split('<Value>')[1:]]
+    dict_rate = dict(zip(charcode, zip(nominal, value)))
+    rate = dict_rate.get(currency.upper())
+    if dict is None:
+        print('Указанная валюта отсутствует')
+    else:
+        print(f'Курс RUB за {rate[0]} {currency.upper()}: {rate[1]}')
+
+
+if __name__ == '__main__':
+    currency_rates('eur')
